ca02a10e5759a3f2b9a02c91df4f0116bf961e06
pbj7198 <pbj7198>
2023-12-27 00:27:54 +0900

    upload

---
+++ lib/screen/main/tab/home/bank_accounts_dummy.dart
@@ -0,0 +1,29 @@
+import 'package:fast_app_base/screen/main/tab/home/banks_dummy.dart';
+import 'package:fast_app_base/screen/main/tab/home/vo/vo_bank_account.dart';
+
+final bankAccountShinhan1 =
+    BankAccount(bankShinhan, 3000000, accountTypeName: '신한 주거래 우대통장(저축예금)');
+final bankAccountShinhan2 =
+    BankAccount(bankShinhan, 30000000, accountTypeName: '저축예금');
+final bankAccountShinhan3 =
+    BankAccount(bankShinhan, 300000000, accountTypeName: '저축예금');
+final bankAccountToss = BankAccount(bankShinhan, 5000000);
+final bankAccountKakao =
+    BankAccount(bankShinhan, 7000000, accountTypeName: '입출금통장');
+
+main(){
+  print(bankAccounts[0].accountTypeName);
+}
+
+// List
+final bankAccounts = [
+  bankAccountShinhan1,
+  bankAccountShinhan2,
+  bankAccountShinhan3,
+  bankAccountToss,
+  bankAccountKakao
+];
+
+// Map
+
+// Set

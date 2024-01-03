95fbdec1a9df5856291d26e551d49bea3a7dfd5d
pbj7198 <pbj7198>
2024-01-04 00:10:15 +0900

    upload

---
+++ lib/screen/notification/w_notification_item.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class NotificationItemWidget extends StatefulWidget {
+  const NotificationItemWidget({super.key});
+
+  @override
+  State<NotificationItemWidget> createState() => _NotificationItemWidgetState();
+}
+
+class _NotificationItemWidgetState extends State<NotificationItemWidget> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
